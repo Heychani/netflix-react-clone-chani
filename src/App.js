6a9194e677b85d18aff3a1c3c799a17cfe693d5c
Chanique Lombard <98527335+Heychani@users.noreply.github.com>
2023-04-17 15:19:58 +0200

    Connecting the database

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './App.css';
 import Row from './Row';
+import requests from './requests';
 
 function App() {
   return (
     <div className="App">
       <h1>Netflix clone front-end</h1>
-      <Row title="NETFLIX ORIGINALS" />
-      <Row title="Trending Now" />
-      <Row title="Comedy" />
-      <Row title="Horrors" />
+      {/* <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} /> */}
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="example" fetchUrl={requests.exampleAPI} />
+      {/* <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
+      <Row title="History" fetchUrl={requests.fetchHistoryMovies}/> */}
     </div>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -1,7 +1,21 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from './axios';
+// import requests from './requests';
 
-function Row({ title }) {
-    const [movies, setMovies] = useState( [] ) //reactHook
+function Row({ title, fetchUrl }) {
+    const [movies, setMovies] = useState([]); //reactHook
+    //snippet of code that runs based on a specific condition/variable
+    useEffect(() => {
+        // if [], run once when the row loads, doesn't run again 
+        async function fetchData() {
+            const request = await axios.get(fetchUrl);
+            //pulls through the url
+            console.log(request.data.results);
+            return request;
+        }
+        fetchData();
+    }, []);
+
     return (
     <div>
         <h2>{title}</h2>
