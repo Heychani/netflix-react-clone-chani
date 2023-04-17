74d04ce7654f31294f8bc1a58c307b7198677f92
Chanique Lombard <98527335+Heychani@users.noreply.github.com>
2023-04-17 16:15:42 +0200

    Movie images are showing

--- src/Row.js
+++ src/Row.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import axios from './axios';
 // import requests from './requests';
 
+const base_url = "https://image.tmdb.org/t/p/original";
+
 function Row({ title, fetchUrl }) {
     const [movies, setMovies] = useState([]); //reactHook
     //snippet of code that runs based on a specific condition/variable
@@ -10,17 +12,24 @@ function Row({ title, fetchUrl }) {
         async function fetchData() {
             const request = await axios.get(fetchUrl);
             //pulls through the url
-            console.log(request.data.results);
+            // console.log(request.data.results); - Testing if the database is showing up and working
+            setMovies(request.data.results);
             return request;
         }
-        fetchData();
-    }, []);
+        fetchData(); //calling the function
+    }, [fetchUrl]); //adding the array
+
+    console.log(movies);
 
     return (
-    <div>
+    <div className="row">
         <h2>{title}</h2>
-
-        {/* container -> posters */}
+        <div className="row__posters">
+                    {/* row__posters */}
+        {movies && movies.map(movie => (
+            <img src={`${base_url}${movie.poster_path}`} alt={movie.name} />
+    ))}
+        </div>
     </div>
   )
 }
